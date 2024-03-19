1bba05c28a32b35e60d011f1d1699023b348e44e
spengre <spencerscottg@gmail.com>
2024-03-19 11:10:47 -0400

    Base server functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,16 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
-const submit = async function( event ) {
+const logCourse = async function( event ) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
   event.preventDefault()
   
-  const input = document.querySelector( "#yourname" ),
-        json = { yourname: input.value },
+  const cID = document.querySelector( "#cID" ),
+        cName = document.querySelector("#cName"),
+        prof = document.querySelector("#prof"),
+        json = { cID: parseInt(cID.value), cName: cName.value, prof: prof.value },
         body = JSON.stringify( json )
 
   const response = await fetch( "/submit", {
@@ -21,7 +23,30 @@ const submit = async function( event ) {
   console.log( "text:", text )
 }
 
+const deleteCourse = async function( event ) {
+  // stop form submission from trying to load
+  // a new .html page for displaying results...
+  // this was the original browser behavior and still
+  // remains to this day
+  event.preventDefault()
+  
+  const cID = document.querySelector( "#removeCID" ),
+        json = { cID: parseInt(cID.value) },
+        body = JSON.stringify( json )
+
+  const response = await fetch( "/delete", {
+    method:"DELETE",
+    body 
+  })
+
+  const text = await response.text()
+
+  console.log( "text:", text )
+}
+
 window.onload = function() {
-   const button = document.querySelector("button");
-  button.onclick = submit;
+  const log = document.querySelector("#log");
+  const remove = document.querySelector("#remove");
+  log.onclick = logCourse;
+  remove.onclick = deleteCourse;
 }
