65f501de90f4a643905d4e89792285c5b97634a0
Nora Cleary <120121240+ngcleary@users.noreply.github.com>
2024-03-20 11:02:01 -0400

    added item and qty to entry object

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,14 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
-const submit = async function( event ) {
+const submit = async function (event) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
-  event.preventDefault()
-  
+  event.preventDefault();
+
+  //This code works - addding additional fields
+  /*
   const input = document.querySelector( "#yourname" ),
         json = { "yourname": input.value },
         body = JSON.stringify( json )
@@ -15,26 +17,45 @@ const submit = async function( event ) {
     method:"POST",
     body 
   })
+*/
+
+  const nameInput = document.querySelector("#yourname");
+  const namejson = { yourname: nameInput.value };
+  //const namebody = JSON.stringify(namejson);
+
+  const itemInput = document.querySelector("#youritem");
+  const itemjson = { youritem: itemInput.value };
+  //const itembody = JSON.stringify(itemjson);
+
+  const qtyInput = document.querySelector("#numItems");
+  const qtyjson = { numItems: qtyInput.value };
+  //const itembody = JSON.stringify(itemjson);
+
+  const response = await fetch("/submit", {
+    method: "POST",
+    body: JSON.stringify(namejson, itemjson, qtyjson),
+  });
 
   const text = await response.text();
-  console.log( "text:", text )
+  console.log("text:", text);
   //document.querySelector("#output").innerHTML = body;
 
-  createEntry(input.value);
-  //call create entry function??
-}
+  createEntry(nameInput.value, itemInput.value, qtyInput.value);
+};
 
-//add later -> const createEntry = function(name, item, numItems){
-const createEntry = function(name){
+const createEntry = function (name, item, qty) {
   const entry = {};
   entry.name = name;
-  //entry.item = item;
-  //entry.qty = qty;
-  document.querySelector("#output").innerHTML = entry.name;
-  return entry;
-}
+  entry.item = item;
+  entry.qty = qty;
+  document.querySelector("#output1").innerHTML = entry.name;
+  document.querySelector("#output2").innerHTML = entry.item;
+  document.querySelector("#output3").innerHTML = entry.qty;
 
-window.onload = function() {
-   const button = document.querySelector("button");
+  return entry;
+};
+
+window.onload = function () {
+  const button = document.querySelector("button");
   button.onclick = submit;
-}
+};
