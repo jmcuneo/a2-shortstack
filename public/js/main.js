d5698e83cfa73d00269f18ae5b7b0bac75837e1e
JackLafond <jacklafo12@gmail.com>
2024-03-21 11:42:20 -0400

    added better comments, emptied forms after updating data to avoid mistakes, added messages to server console log

--- public/js/main.js
+++ public/js/main.js
@@ -19,9 +19,15 @@ const submit = async function( event ) {
   })
   const text = await response.text()
   console.log(text)
+  emptyForms()
   loadData()
 }
 
+
+
+/**
+ * function to get app data from server
+ */
 async function loadData() {
  
   const response = await fetch( "/appdata", {
@@ -29,15 +35,54 @@ async function loadData() {
   })
   const text = await response.text()
   console.log("loaded data")
+
+  // if data is sent back fill table with data
   fillTable(text);
 }
 
+
+
+/**
+ * function to empty submit and update forms
+ */
+async function emptyForms() {
+  const sname = document.querySelector( "#yourname" ),
+        sbday = document.querySelector( "#yourbday" ),
+        scake = document.querySelector( "#yourcake" )
+
+  sname.value = ''
+  sbday.value = ''
+  scake.value = ''
+
+  const uid = document.querySelector( "#id" ),
+        uname = document.querySelector( "#name" ),
+        uage = document.querySelector( "#age" ),
+        ubday = document.querySelector( "#bday" ),
+        ucake = document.querySelector( "#cake" )
+
+  uid.value = ''
+  uname.value = ''
+  uage.value = ''
+  ubday.value = ''
+  ucake.value = ''
+}
+
+
+
+/**
+ * function to fill table with data
+ * @param {*} text the app data sent from server in string form
+ */
 async function fillTable(text) {
 
+  // get table and empty it
   var table = document.querySelector(" #datatable ");
   table.innerHTML = '';
+
+  // parse data
   const data = JSON.parse(text);
 
+  // iterate over each entry and add a new table row for each entry, new table cell for each attribute
   for(const elt of data) {
 
     var row = table.insertRow();
@@ -51,10 +96,16 @@ async function fillTable(text) {
     bdaycell.id = "bdaycell";
     var cakecell = row.insertCell();
     cakecell.id = "cakecell";
+
+    // for each entry add a delete button for that entry
     var del = document.createElement('button');
     del.type = "button";
     del.innerHTML = "delete";
+
+    // set onclick event to call delete function
     del.onclick = (function(elt) {return function() {delEntry(elt);}})(elt);
+
+    // add new elements and set their values
     row.appendChild(del);
 
     idcell.innerHTML = elt.id;
@@ -63,11 +114,20 @@ async function fillTable(text) {
     bdaycell.innerHTML = elt.birthday;
     cakecell.innerHTML = elt.preferredCake;
 
+    // set on click for each id cell to call fill entry function
     idcell.onclick = (function(elt) {return function() {fillEntry(elt);}})(elt);
   }
 }
 
+
+
+/**
+ * function to delete an entry from app data
+ * @param {*} elt the entry to delete
+ */
 const delEntry = async function(elt) {
+
+  // get the id to delete, add it to request, and make delete request to server
   const id = elt.id,
         json = { "id": id },
         body = JSON.stringify( json )
@@ -76,18 +136,30 @@ const delEntry = async function(elt) {
     method:"DELETE",
     body 
   })
+
+  // get server response and reload updated data
   const text = await response.text()
   console.log(text)
+  emptyForms()
   loadData()
 }
 
+
+
+/**
+ * function to fill the update form with an entry
+ * @param {*} elt the entry to fill into the update form
+ */
 const fillEntry = async function(elt) {
+
+  // get the html elements of the form
   const id = document.querySelector( "#id" ),
         name = document.querySelector( "#name" ),
         age = document.querySelector( "#age" ),
         bday = document.querySelector( "#bday" ),
         cake = document.querySelector( "#cake" )
 
+  // update the element values to the selected entry values
   id.value = elt.id
   name.value = elt.name
   age.value = elt.age
@@ -95,9 +167,18 @@ const fillEntry = async function(elt) {
   cake.value = elt.preferredCake
 }
 
+
+
+/**
+ * function to udpate an entry in app data
+ * @param {*} event 
+ */
 const updateEntry = async function( event ) {
+
+  // don't allow enter to reload page
   event.preventDefault();
 
+  // get the html elements of the update form, then create a new object and stringify it
   const id = document.querySelector( "#id" ),
         name = document.querySelector( "#name" ),
         age = document.querySelector( "#age" ),
@@ -105,17 +186,23 @@ const updateEntry = async function( event ) {
         cake = document.querySelector( "#cake" ),
         json = { id: parseInt(id.value), name: name.value, age: parseInt(age.value), birthday: bday.value, preferredCake: cake.value},
         body = JSON.stringify( json )
-  console.log(json)
 
+  // send string as a PATCH request to server
   const response = await fetch( "", {
     method:"PATCH",
     body 
   })
+
+  // get reponse from server and reload updated data
   const text = await response.text()
   console.log(text)
+  emptyForms()
   loadData()
 }
 
+
+
+// on window load, set submit and update button onclick events, then load the app data
 window.onload = function() {
   const submitBtn = document.querySelector("button#submit");
   submitBtn.onclick = submit;
--- server.improved.js
+++ server.improved.js
@@ -8,12 +8,27 @@ const http = require( "http" ),
       dir  = "public/",
       port = 3000
 
+
+
+/**
+ * Base App Data
+ * One entry represents a birthday entry
+ * Name is person's name, 
+ * birthday is date of bday in MM/DD/YYY form, 
+ * preferred cake is their preferred flavor of cake,
+ * age and id are calculated fields
+ */
 const appdata = [
   { "name": "Jack", "birthday": "05/24/2002", "preferredCake": "chocolate", "age": 21, "id": 0 },
   { "name": "Jim", "birthday": "10/13/1938", "preferredCake": "vanilla", "age": 85, "id": 1 },
   { "name": "John", "birthday": "07/18/1967", "preferredCake": "swirl", "age": 56, "id": 2 } 
 ]
 
+
+
+/**
+ * Server to send requests to proper handler
+ */
 const server = http.createServer( function( request,response ) {
   if( request.method === "GET" ) {
     handleGet( request, response )    
@@ -26,6 +41,13 @@ const server = http.createServer( function( request,response ) {
   }
 })
 
+
+
+/**
+ * GET handler
+ * @param {*} request is a server request
+ * @param {*} response is the server reponse
+ */
 const handleGet = function( request, response ) {
   const filename = dir + request.url.slice( 1 ) 
 
@@ -38,6 +60,13 @@ const handleGet = function( request, response ) {
   }
 }
 
+
+
+/**
+ * POST handler
+ * @param {*} request is a server request
+ * @param {*} response is the server reponse
+ */
 const handlePost = function( request, response ) {
   let dataString = ""
 
@@ -48,9 +77,10 @@ const handlePost = function( request, response ) {
   request.on( "end", function() {
 
     var entry = JSON.parse( dataString );
+    console.log("POST")
     console.log( entry )
 
-    // ... do something with the data here!!!
+    // for an entry we need to parse the bday string and then calculate age
     const bdayString = entry.birthday;
     const bdayParts = bdayString.split("/");
     const bday = new Date(parseInt(bdayParts[2]), parseInt(bdayParts[1]) - 1, parseInt(bdayParts[0]));
@@ -59,16 +89,25 @@ const handlePost = function( request, response ) {
     const age = today.getFullYear() - bday.getFullYear();
     entry.age = age;
 
+    // set id to length since it will be the latest entry
     const id = appdata.length;
     entry.id = id;
 
     appdata.push(entry);
 
+    // write reponse back
     response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
     response.end("successfully added new entry")
   })
 }
 
+
+
+/**
+ * DELETE handler
+ * @param {*} request a server request
+ * @param {*} response the server response
+ */
 const handleDelete = function( request, response ) {
   let dataString = ""
 
@@ -79,16 +118,22 @@ const handleDelete = function( request, response ) {
   request.on( "end", function() {
 
     var entry = JSON.parse( dataString );
+    console.log("DELETE")
     console.log( entry )
 
+    // get the id of the entry to delete and check that it is a valid id
     const id = entry.id;
 
-    if(id > -1 & id < appdata.length) {
+    if(id > -1 & id < appdata.length & !(id===null)) {
+
+      // if valid id, delete it and reset the ids, then send back response
       appdata.splice(id, 1);
       resetIDs();
       response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
       response.end("successfully removed entry")
     } else {
+
+      // if invalid id send error back
       response.writeHeader( 404 )
       response.end( "404 Error: Entry Not Found" )
     }
@@ -96,6 +141,13 @@ const handleDelete = function( request, response ) {
   })
 }
 
+
+
+/**
+ * PATCH handler
+ * @param {*} request 
+ * @param {*} response 
+ */
 const handlePatch = function ( request, response ) {
 
   let dataString = ""
@@ -107,13 +159,18 @@ const handlePatch = function ( request, response ) {
   request.on( "end", function() {
 
     var entry = JSON.parse( dataString );
+    console.log("PATCH")
     console.log( entry )
 
+    // get id of entry to update then check that it is a valid id
     const id = entry.id;
 
-    if(id > -1 & id < appdata.length) {
+    if(id > -1 & id < appdata.length & !(id===null)) {
+
+      // if it is a valid id, update the current entry to the new entry
       appdata[id] = entry;
 
+      // recalculate age
       const bdayString = entry.birthday;
       const bdayParts = bdayString.split("/");
       const bday = new Date(parseInt(bdayParts[2]), parseInt(bdayParts[1]) - 1, parseInt(bdayParts[0]));
@@ -122,9 +179,12 @@ const handlePatch = function ( request, response ) {
       const age = today.getFullYear() - bday.getFullYear();
       appdata[id].age = age;
 
+      // send back response
       response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
       response.end("successfully updated entry")
     } else {
+
+      // if invalid id send error back
       response.writeHeader( 404 )
       response.end( "404 Error: Entry Not Found" )
     }
@@ -133,6 +193,12 @@ const handlePatch = function ( request, response ) {
 }
 
 
+
+/**
+ * helper function for GET to send files
+ * @param {*} response the server response
+ * @param {*} filename the file name to return
+ */
 const sendFile = function( response, filename ) {
    const type = mime.getType( filename ) 
 
@@ -155,13 +221,25 @@ const sendFile = function( response, filename ) {
    })
 }
 
+
+
+/**
+ * helper function for GET to send data
+ * @param {*} response the server response
+ */
 const sendData = function( response ) {
 
+  // add data to response
   response.writeHeader( 200, { "Content-Type": "Text" })
   response.end(JSON.stringify(appdata));
 
 }
 
+
+
+/**
+ * function to reset IDs after data changes, each id is just the position of each entry in the list of data
+ */
 const resetIDs = async function() {
   const len = appdata.length;
   for(let i = 0; i < len; i++) {
