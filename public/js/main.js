e3e30ba3b808058e0a59dfc2dda576fe203e891d
Zesheng Chen <113056866+wingmancover@users.noreply.github.com>
2024-03-21 20:26:32 -0400

    updated submit function

--- public/js/main.js
+++ public/js/main.js
@@ -6,19 +6,20 @@ const submit = async function( event ) {
   // this was the original browser behavior and still
   // remains to this day
   event.preventDefault()
-  
-  const input = document.querySelector( "#yourname" ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+
+  const playerName = document.querySelector("#playerName").value,
+      score = document.querySelector("#score").value,
+      gameDate = document.querySelector("#gameDate").value,
+      json = {playerName, score: Number(score), gameDate},
+      body = JSON.stringify(json);
 
   const response = await fetch( "/submit", {
     method:"POST",
+    headers:{'Content-Type': 'application/json'},
     body 
-  })
-
-  const text = await response.text()
-
-  console.log( "text:", text )
+  }).then(response => response.json())
+      .then(json => console.log(json));
+  // Here need to update the client-side display to show the new dataset
 }
 
 window.onload = function() {
