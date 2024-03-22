077bb89e3ff8225348f6753e0a54d5019ab69b46
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2024-03-22 09:58:07 -0400

    some changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
+let tableIndex = 1;
+
 const submit = async function( event ) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
@@ -20,7 +22,7 @@ const submit = async function( event ) {
 
   console.log( "text:", text )
 
-  insetElementToTable(text)
+  insetElementToTable(input.value)
 
 }
 
@@ -35,9 +37,21 @@ window.onload = function() {
 //CUSTOM CODE: Add this to the table
 function insetElementToTable(text)
 {
+  tableIndex++;
   const table = document.getElementById("InformationTable")
-  let row = table.insertRow(1)
-  let cell = row.insertCell(0)
-  console.log(text)
-  cell.innerHTML = text;
+  let row = table.insertRow(tableIndex)
+
+  for(let i = 0; i <= 3; i++)
+  {
+    let cell = row.insertCell(i)
+
+    if(i == 0)
+    {
+      cell.innerHTML = text;
+    }
+    cell.innerHTML = text;
+  }
+
+
+
 }
