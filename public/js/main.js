7d6281798bcbb0a59d69bc0307b42ed98d5e95d4
yuranxue272181 <xue272181@gmail.com>
2024-03-23 14:02:42 -0400

    mpdified

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,6 @@
-// FRONT-END (CLIENT) JAVASCRIPT HERE
 
 
-//
-// const result = async function( event ) {
-//   event.preventDefault()
-//   await fetch( "/results", {
-//     method:"POST",
-//   }).then(
-//   function(response){
-//     return response.text()
-//   })  .then(data => {
-//     // Handle the response based on its content type
-//     if (typeof data === 'string') {
-//       document.body.innerHTML = data
-//     } else {
-//       console.log(data);
-//     }
-//   })
-//
-// }
+
 const submit = async function( event ) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
@@ -39,9 +21,12 @@ const submit = async function( event ) {
 }
 
 
-  window.onload = function (){
-  const button1 = document.getElementById("submitBtn");
-  button1.onclick = submit;
-  const button2 = document.getElementById("resultBtn");
-  button2.onclick = result;
+
+
+  window.onload = function () {
+    const button1 = document.getElementById("submitBtn");
+    button1.onclick = submit;
+    const button2 = document.getElementById("resultBtn");
+    button2.onclick = result;
   }
+
--- server.improved.js
+++ server.improved.js
@@ -1,40 +1,56 @@
 const http = require( "http" ),
-      fs   = require( "fs" ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library if you"re testing this on your local machine.
-      // However, Glitch will install it automatically by looking in your package.json
-      // file.
-      mime = require( "mime" ),
-      dir  = "public/",
-      port = 3000
+    fs   = require( "fs" ),
+    // IMPORTANT: you must run `npm install` in the directory for this assignment
+    // to install the mime library if you"re testing this on your local machine.
+    // However, Glitch will install it automatically by looking in your package.json
+    // file.
+    mime = require( "mime" ),
+    dir  = "public/",
+    port = 3000
 
 const appdata = [
-  { "model": "toyota", "year": 1999, "mpg": 23 },
-  { "model": "honda", "year": 2004, "mpg": 30 },
-  { "model": "ford", "year": 1987, "mpg": 14} 
+    { "model": "toyota", "year": 1999, "mpg": 23 },
+    { "model": "honda", "year": 2004, "mpg": 30 },
+    { "model": "ford", "year": 1987, "mpg": 14}
 ]
 
 const server = http.createServer( function( request,response ) {
-  if( request.method === "GET" ) {
-    handleGet( request, response )    
-  }else if( request.method === "POST" ){
-    handlePost( request, response ) 
-  }
+    if( request.method === "GET" ) {
+        handleGet( request, response )
+    }else if( request.method === "POST" ){
+        handlePost( request, response )
+    }else if (request.method === "DELETE"){
+        handleDelete( request, response)
+    }
 })
+
+const handleDelete = function( request, response ) {
+    //write your code here
+}
 
 const handleGet = function( request, response ) {
 
-  const filename = dir + request.url.slice( 1 ) 
+    const filename = dir + request.url.slice( 1 )
 
-  if( request.url === "/" ) {
-    sendFile( response, "public/index.html" )
-  }else{
-    sendFile( response, filename )
-  }
+    if(request.url === '/public/index.html'){
+        let dataString = "";
+
+        request.on("data", function (data) {
+            dataString += data;
+        });
+        request.on("end", function () {
+            // console.log(appdata)
+            response.writeHead(200, "OK", {"Content-Type": "text/plain"})
+            response.end(JSON.stringify(appdata))})}
+    else if( request.url === "/" ) {
+        sendFile( response, "public/index.html" )
+    }else{
+        sendFile( response, filename )
+    }
 }
 
 const handlePost = function( request, response ) {
-    if(request.url === '/submit'){
+    if (request.url === '/submit') {
         let dataString = ""
 
         request.on("data", function (data) {
@@ -47,55 +63,27 @@ const handlePost = function( request, response ) {
             response.end("test")
         })
     }
-    if(request.url === '/public/index.html'){
-        let dataString = "";
-
-        request.on("data", function (data) {
-            dataString += data;
-        });
-        request.on("end", function () {
-           // console.log(appdata)
-            response.writeHead(200, "OK", {"Content-Type": "text/plain"})
-            response.end(JSON.stringify(appdata))})}}
-      //   const html = `
-      // <html lang="en">
-      //   <head>
-      //   <meta charset="UTF-8">
-      //   </head>
-      //   <body>
-      //   <head>
-      //   <tr>AppData:<br> </tr>
-      //   </head>
-      //   <tbody>
-      //   ${appdata.map(item => JSON.stringify(item))}
-      //   </tbody>
-      //   </body>
-      //   </html>
-      // `
-      // response.writeHead(200, "OK", {"Content-Type": "text/plain"})
-      // response.end(html)
-
-
+}
 const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+    const type = mime.getType( filename )
 
-   fs.readFile( filename, function( err, content ) {
+    fs.readFile( filename, function( err, content ) {
 
-     // if the error = null, then we"ve loaded the file successfully
-     if( err === null ) {
+        // if the error = null, then we"ve loaded the file successfully
+        if( err === null ) {
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { "Content-Type": type })
-       response.end( content )
+            // status code: https://httpstatuses.com
+            response.writeHeader( 200, { "Content-Type": type })
+            response.end( content )
 
-     }else{
+        }else{
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( "404 Error: File Not Found" )
+            // file not found, error code 404
+            response.writeHeader( 404 )
+            response.end( "404 Error: File Not Found" )
 
-     }
-   })
+        }
+    })
 }
 
 server.listen( process.env.PORT || port )
