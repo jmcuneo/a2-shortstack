548fda0ba0a6e5ecccca0dce2e8fbe862914ff5f
brsahagian <44908953+brsahagian@users.noreply.github.com>
2024-03-23 23:14:48 -0400

    First Draft Site

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,71 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
-const submit = async function( event ) {
+const add = async function(event){
   // stop form submission from trying to load
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
   event.preventDefault()
   
-  const input = document.querySelector( "#yourname" ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+  const input = document.querySelectorAll("#add_part, #add_material, #add_quantity, #add_weight"),
+        json = {part_name: input.item(0).value, new_material: input.item(1).value, new_quantity: input.item(2).value,
+        weight_per_unit: input.item(3).value, type: "add"},
+        body = JSON.stringify(json)
 
   const response = await fetch( "/submit", {
     method:"POST",
     body 
   })
-
   const text = await response.text()
+  console.log( "Add:", text )
+  constructTable()
+}
 
-  console.log( "text:", text )
+const remove = async function(event){
+  event.preventDefault()
+
+  const input = document.querySelector("#remove_part"),
+        json = {part_name: input.value, type: "remove"},
+        body = JSON.stringify(json)
+
+  const response = await fetch( "/submit", {
+    method:"POST",
+    body 
+  })
+  const text = await response.text()
+  console.log( "Remove:", text )
+  constructTable()
+}
+
+const receive = async function() {
+  file = "/getarr"
+  const filename = "public/" + file.slice( 1 )
+  console.log(filename)
+  const response = await fetch ("/getarr", {method:"GET"})
+
+  const entries = await response.text()
+  console.log("table:", entries)
+  return JSON.parse(entries)
+}
+
+const constructTable = async function() {
+  entries = await receive()
+  console.log(entries)
+  htmlString = ""
+  for(let i = 0; i < entries.length; i++){
+    row = "<tr><td>" +  entries[i].Part + "</td><td>" + entries[i].Material + "</td><td>" + entries[i].Quantity + "</td><td>" + entries[i].Weight + "</td></tr>"
+    htmlString += row
+    console.log(htmlString)
+  }
+
+  table_body = document.getElementById("table_body")
+  table_body.innerHTML = htmlString
 }
 
 window.onload = function() {
-   const button = document.querySelector("button");
-  button.onclick = submit;
+  const add_button = document.getElementById("add_button");
+  const remove_button = document.getElementById("remove_button");
+  add_button.onclick = add;
+  remove_button.onclick = remove;
+  constructTable()
 }
--- server.improved.js
+++ server.improved.js
@@ -9,9 +9,9 @@ const http = require( "http" ),
       port = 3000
 
 const appdata = [
-  { "model": "toyota", "year": 1999, "mpg": 23 },
-  { "model": "honda", "year": 2004, "mpg": 30 },
-  { "model": "ford", "year": 1987, "mpg": 14} 
+  { "Part": "Spinner", "Material": "Steel", "Quantity": 1, "Weight": 0.702084 },
+  { "Part": "Back Plate", "Material": "Aluminum", "Quantity": 1, "Weight": 0.0380388 },
+  { "Part": "Uprights", "Material": "UHML", "Quantity": 2, "Weight": 0.0871949} 
 ]
 
 const server = http.createServer( function( request,response ) {
@@ -23,30 +23,52 @@ const server = http.createServer( function( request,response ) {
 })
 
 const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
-
-  if( request.url === "/" ) {
-    sendFile( response, "public/index.html" )
+  const filename = dir + request.url.slice(1)
+  if(request.url === "/") {
+    sendFile(response, "public/index.html")
+  }else if(request.url === "/getarr"){
+    sendArr(response, appdata)
   }else{
-    sendFile( response, filename )
+    sendFile(response, filename)
   }
 }
 
 const handlePost = function( request, response ) {
   let dataString = ""
 
-  request.on( "data", function( data ) {
+  request.on("data", function(data){
       dataString += data 
   })
 
   request.on( "end", function() {
-    console.log( JSON.parse( dataString ) )
-
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
+    var entry = JSON.parse(dataString)
+    entry_index = arrayIndexOf(entry.part_name, appdata)
+    if(entry.type === "add"){
+      new_quantity = parseFloat(entry.new_quantity)
+      weight_per_unit = parseFloat(entry.weight_per_unit)
+      new_entry = {"Part": entry.part_name, "Material": entry.new_material, 
+      "Quantity": entry.new_quantity, "Weight": entry.weight_per_unit * entry.new_quantity}
+      if(entry_index == appdata.length && (new_quantity > 0 && weight_per_unit > 0)){
+        appdata.push(new_entry)
+      }else if(new_quantity > 0 && weight_per_unit > 0){
+        appdata.splice(entry_index, 1, new_entry)
+      }
+    }else{
+      appdata.splice(arrayIndexOf(entry.part_name, appdata), 1)
+    }
+    console.log(appdata)
+    response.writeHead( 200, "OK", {"Content-Type": "text/plain"})
     response.end("test")
   })
+}
+
+function arrayIndexOf(part_name, arr){
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i].Part === part_name){
+      return i;
+    }
+  }
+  return arr.length;
 }
 
 const sendFile = function( response, filename ) {
@@ -54,7 +76,7 @@ const sendFile = function( response, filename ) {
 
    fs.readFile( filename, function( err, content ) {
 
-     // if the error = null, then we"ve loaded the file successfully
+     // if the error = null, then we've loaded the file successfully
      if( err === null ) {
 
        // status code: https://httpstatuses.com
@@ -69,6 +91,11 @@ const sendFile = function( response, filename ) {
 
      }
    })
+}
+
+const sendArr = function(response, arr){
+  response.writeHeader(200, "Fetched Table")
+  response.end(JSON.stringify(arr))
 }
 
 server.listen( process.env.PORT || port )
