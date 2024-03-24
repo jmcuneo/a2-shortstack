bf7d57b03b178aef02dd3f9676051bba99319da2
dovushman <52638904+dovushman@users.noreply.github.com>
2024-03-24 19:42:27 -0400

    a2 finished project, readMe needs to be finished

--- server.improved.js
+++ server.improved.js
@@ -11,23 +11,32 @@ const http = require( "http" ),
 const appdata = [
   { "model": "toyota", "year": 1999, "mpg": 23 },
   { "model": "honda", "year": 2004, "mpg": 30 },
-  { "model": "ford", "year": 1987, "mpg": 14} 
+  { "model": "ford", "year": 1987, "mpg": 14}
 ]
+
+let namesArray = []
 
 const server = http.createServer( function( request,response ) {
   if( request.method === "GET" ) {
-    handleGet( request, response )    
+    handleGet( request, response )
   }else if( request.method === "POST" ){
-    handlePost( request, response ) 
+    handlePost( request, response )
+  }else if (request.method === "DELETE"){
+    handleDelete(request, response)
   }
+
 })
 
 const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+  const filename = dir + request.url.slice( 1 )
 
   if( request.url === "/" ) {
     sendFile( response, "public/index.html" )
-  }else{
+  } else if (request.url === "/data"){
+    response.writeHead( 200, "OK", {"Content-Type": "application/json" })
+    response.end(JSON.stringify(namesArray))
+  }
+  else{
     sendFile( response, filename )
   }
 }
@@ -36,21 +45,65 @@ const handlePost = function( request, response ) {
   let dataString = ""
 
   request.on( "data", function( data ) {
-      dataString += data 
+      dataString += data
   })
 
   request.on( "end", function() {
-    console.log( JSON.parse( dataString ) )
-
     // ... do something with the data here!!!
 
-    response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
-    response.end("test")
+    const formData = JSON.parse(dataString);
+
+    const id = formData.id;
+    const firstName = formData.firstName;
+    const middleName = formData.middleName;
+    const lastName = formData.lastName;
+    const initial = (firstName.charAt(0) + middleName.charAt(0) + lastName.charAt(0)).toUpperCase();
+
+
+    const nameJson =  {
+      id: id,
+      firstName: firstName,
+      middleName: middleName,
+      lastName: lastName,
+      initial: initial
+    }
+
+    namesArray.push(nameJson)
+
+      console.log(namesArray)
+
+      response.writeHead(200, "OK", {"Content-Type": "application/json"})
+      response.end(JSON.stringify(nameJson))
+    })
+  }
+
+const handleDelete = function( request, response ) {
+  let dataString = ""
+
+  request.on( "data", function( data ) {
+    dataString += data
+  })
+
+  request.on( "end", function() {
+    const parsedDataString = JSON.parse(dataString);
+    // const idToDelete = parsedDataString.id;
+    const idToDelete = parseInt(parsedDataString.id);
+
+    for (let i = 0; i < namesArray.length; i++){
+      if (idToDelete === namesArray[i].id){
+        namesArray.splice(i, 1)
+      }
+    }
+
+    console.log(namesArray)
+
+    response.writeHead(200, "OK", {"Content-Type": "application/json"})
+    response.end(JSON.stringify({status: 'success', message: 'Delete'}))
   })
 }
 
 const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+   const type = mime.getType( filename )
 
    fs.readFile( filename, function( err, content ) {
 
