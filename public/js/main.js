4a3b31d6480b40338439ec9905c4629bc2f86f31
Darneur <20408795+Darneur@users.noreply.github.com>
2024-03-23 21:21:59 -0400

    post-montage

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,156 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
+// Submit the form data to the server
 const submit = async function( event ) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
   event.preventDefault()
+
+  const dateString = document.getElementById("date"),
+    creationDate = new Date(dateString.value),
+    creationDateFormatted = creationDate.toISOString().slice(0, 10);
   
-  const input = document.querySelector( "#yourname" ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+  const name = document.getElementById("name"),
+    description = document.getElementById("description"),
+    priority = document.getElementById("priority"),
+    json = {Name: name.value, Description: description.value, "Creation Date": creationDateFormatted, Priority: priority.value},
+    body = JSON.stringify(json)
 
   const response = await fetch( "/submit", {
     method:"POST",
     body 
   })
 
-  const text = await response.text()
+  if(response.ok) {
+    console.log("Data submitted successfully");
+  } else {
+    console.log("Error: ", response.status, response.statusText);
+    alert("Error: " + response.status + " " + response.statusText);
+  }
 
-  console.log( "text:", text )
+
+  //const text = await response.text()
+  //console.log("text:", text);
+  getResults(event);
 }
 
+// Delete form data with the value of the key (name)
+async function deleteData(event) {
+  event.preventDefault();
+  const name = document.getElementById("name"),
+        json = {"Name": name.value},
+        body = JSON.stringify(json);
+
+  const response = await fetch("/delete", {
+    method: "POST",
+    body
+  });
+
+  if(response.ok) {
+    console.log("Data deleted successfully");
+  } else {
+    console.log("Error: ", response.status, response.statusText);
+    alert("Error: " + response.status + " " + response.statusText);
+  }
+  getResults(event);
+}
+
+async function editData(event) {
+  event.preventDefault();
+  const date = new Date(document.getElementById("date").value);
+  const name = document.getElementById("name"),
+        description = document.getElementById("description"),
+        creationDate = date.toISOString().slice(0,10),
+        priority = document.getElementById("priority"),
+        json = {Name: name.value, Description: description.value, "Creation Date": creationDate, Priority: priority.value},
+        body = JSON.stringify(json);
+
+  const response = await fetch("/edit", {
+    method: "POST",
+    body
+  });
+
+  if(response.ok) {
+    console.log("Data edited successfully");
+  } else {
+    console.log("Error: ", response.status, response.statusText);
+    alert("Error: " + response.status + " " + response.statusText);
+  }
+  getResults(event);
+}
+
+// Get the results of stored data from the server
+async function getResults(event) {
+  event.preventDefault();
+  const response = await fetch("/data.json", {
+    method: "GET"
+  });
+  const text = await response.text();
+  //console.log(text);
+  const data = JSON.parse(text);
+
+  // Display data on table
+  let table = document.getElementById("table");
+  table.innerHTML = "";
+  let header = table.createTHead();
+  let row = header.insertRow(0);
+  let cell = row.insertCell(0);
+  cell.textContent = "Name";
+  cell = row.insertCell(1);
+  cell.textContent = "Description";
+  cell = row.insertCell(2);
+  cell.textContent = "Creation Date";
+  cell = row.insertCell(3);
+  cell.textContent = "Priority";
+  cell = row.insertCell(4);
+  cell.textContent = "Recommended Deadline";
+  for (let i = 0; i < data.length; i++) {
+    row = table.insertRow(i+1);
+    cell = row.insertCell(0);
+    cell.textContent = data[i].Name;
+    cell = row.insertCell(1);
+    cell.textContent = data[i].Description;
+    cell = row.insertCell(2);
+    cell.textContent = data[i]["Creation Date"];
+    cell = row.insertCell(3);
+    cell.textContent = data[i].Priority;
+    cell = row.insertCell(4);
+    cell.textContent = data[i]["Recommended Deadline"];
+  }
+  
+
+}
+
+// Load functions when the window loads
 window.onload = function() {
-   const button = document.querySelector("button");
-  button.onclick = submit;
+  
+  const submitButton = document.getElementById("starter");
+
+  // Make sure form is valid when submitted
+  submitButton.addEventListener('click', function(event) {
+    let form = document.querySelector('form');
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      alert('Please fill out all required fields correctly.');
+    } else {
+      submit(event);
+    }
+  });
+
+  const deleteButton = document.getElementById("delete");
+  deleteButton.onclick = deleteData;
+  const resultButton = document.getElementById("getResults");
+  resultButton.onclick = getResults;
+  const editButton = document.getElementById("edit");
+  editButton.addEventListener('click', function(event) {
+    let form = document.querySelector('form');
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      alert('Please fill out all required fields correctly.');
+    } else {
+      editData(event);
+    }
+  });
 }
--- server.improved.js
+++ server.improved.js
@@ -9,10 +9,20 @@ const http = require( "http" ),
       port = 3000
 
 const appdata = [
-  { "model": "toyota", "year": 1999, "mpg": 23 },
-  { "model": "honda", "year": 2004, "mpg": 30 },
-  { "model": "ford", "year": 1987, "mpg": 14} 
+  { "Name": "think about toyota", "Description": "not to be confused with toy yoda", "Creation Date": "2024-03-13", "Priority": 1, "Recommended Deadline": "2024-03-14"},
+  { "Name": "discuss honda", "Description": "honk honk", "Creation Date": "2024-03-13", "Priority": 2, "Recommended Deadline": "2024-03-15"},
+  { "Name": "get a chicken", "Description": "i need a chicken so bad", "Creation Date": "2024-03-13", "Priority": 3, "Recommended Deadline": "2024-03-16"} 
 ]
+
+// Write appdata to JSON file
+function writeData() {
+  fs.writeFileSync("public/data.json", JSON.stringify(appdata), err => {
+    if (err) throw err;
+    console.log("Data written to file");
+  });
+}
+writeData();
+
 
 const server = http.createServer( function( request,response ) {
   if( request.method === "GET" ) {
@@ -34,18 +44,80 @@ const handleGet = function( request, response ) {
 
 const handlePost = function( request, response ) {
   let dataString = ""
-
   request.on( "data", function( data ) {
       dataString += data 
   })
-
   request.on( "end", function() {
-    console.log( JSON.parse( dataString ) )
+    console.log(JSON.parse(dataString))
 
-    // ... do something with the data here!!!
+    // If the request is to submit data, parse the data and add it to the appdata array
+    if (request.url === "/submit") {
+      let data = JSON.parse(dataString);
+      // Check if the data already exists in the array. If it is, return an error
+      for (let i = 0; i < appdata.length; i++) {
+        if (appdata[i].Name === data.Name) {
+          response.writeHead( 400, "Name already exists", {"Content-Type": "text/plain" })
+          response.end("Name already exists");
+          return
+        }
+      }
+      
+      // Derive the recommended deadline from the priority and creation date
+      let date = new Date(data["Creation Date"]);
+      let recommendedDeadline = new Date(date);
+      recommendedDeadline.setDate(date.getDate() + +data.Priority);
+      data["Recommended Deadline"] = recommendedDeadline.toISOString().slice(0, 10);
 
-    response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
-    response.end("test")
+
+      appdata.push(data);
+      writeData();
+      response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
+      response.end("Data submitted successfully");
+      
+    }
+
+    // If the request is to delete data, parse the data and delete the data from the appdata array
+    if (request.url === "/delete") {
+      let data = JSON.parse(dataString);
+      let found = false;
+      // Look in array for data with the same name and remove it
+      for (let i = 0; i < appdata.length; i++) {
+        if (appdata[i].Name === data.Name) {
+          found = true;
+          appdata.splice(i, 1);
+          break;
+        }
+      }
+      writeData();
+      if (found) {
+        response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
+        response.end("Data deleted successfully");
+      } else {
+        response.writeHead( 400, "Name not found", {"Content-Type": "text/plain" })
+        response.end("Name not found");
+      }
+    }
+
+    if (request.url === "/edit") {
+      let data = JSON.parse(dataString);
+
+      for (let i = 0; i < appdata.length; i++) {
+        if (appdata[i].Name === data.Name) {
+          // Derive the recommended deadline from the priority and creation date
+          let recommendedDeadline = new Date(data["Creation Date"]);
+          recommendedDeadline.setDate(recommendedDeadline.getDate() + +data.Priority);
+          data["Recommended Deadline"] = recommendedDeadline.toISOString().slice(0, 10);
+          appdata[i] = data;
+          writeData();
+          response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
+          response.end("Data edited successfully");
+          return;
+        }
+      }
+
+      response.writeHead( 400, "Name not found", {"Content-Type": "text/plain" })
+      response.end("Name not found");
+    }
   })
 }
 
