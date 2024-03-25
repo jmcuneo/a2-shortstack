bb4a8d9fe682b4df682d177a4c6eddf2b70355ea
pfrizz <109550097+pfrizz@users.noreply.github.com>
2024-03-24 21:56:55 -0400

    Got core features working + some extras

--- README.md
+++ README.md
@@ -87,11 +87,18 @@ You'll need to use sometype of collaborative software that will enable you both 
 Sample Readme (delete the above when you're ready to submit, and modify the below so with your links and descriptions)
 ---
 
-## Your Web Application Title
-Include a very brief summary of your project here. Be sure to include the CSS positioning technique you used, and any required instructions to use your application.
+## Simple To-Do List
+For my project I made a fairly simple to-do list app. The user can input a task name, due date, and color, and an entry will be added to the list on the server.
+The server side calculation determines the number in the priority column of the list. The priority is simply determined by sorting all the items in the list by their due date, with the item at priority 1 being the next item due. 
+For styling I used CSS flexboxes and a variety of selectors as outlined in the assignment description to style all the main visual elements on the page. I also used the Nunito font from Google Fonts.
 
 ## Technical Achievements
-- **Tech Achievement 1**: Using a combination of...
+- **Single-Page App**: Both the form for adding new tasks and the to-do list (the "results" of the form) are displayed side-by-side using CSS flexboxes. The to-do list automatically updates after a submission to the form is made (the user doesn't have to refresh the page).
 
 ### Design/Evaluation Achievements
-- **Design Achievement 1**: 
+- **Customizable Task Coloring**: The color a user selects for a task is reflected in the styling of it's row, which was a little tricky to get properly working.
+- **Sound Effects!**: Little sound effects play when you add or remove a task. Isn't that neat! I'd never used audio in JavaScript before so I had to learn a little bit about that to get this working.
+    - Attributions: 
+        click by 1bob -- https://freesound.org/s/717365/ -- License: Creative Commons 0
+        ui-submit.wav by StavSounds -- https://freesound.org/s/701704/ -- License: Creative Commons 0
+
--- server.improved.js
+++ server.improved.js
@@ -8,11 +8,7 @@ const http = require( "http" ),
       dir  = "public/",
       port = 3000
 
-const appdata = [
-  { "model": "toyota", "year": 1999, "mpg": 23 },
-  { "model": "honda", "year": 2004, "mpg": 30 },
-  { "model": "ford", "year": 1987, "mpg": 14} 
-]
+let appdata = []
 
 const server = http.createServer( function( request,response ) {
   if( request.method === "GET" ) {
@@ -20,14 +16,34 @@ const server = http.createServer( function( request,response ) {
   }else if( request.method === "POST" ){
     handlePost( request, response ) 
   }
+  else if (request.method === "DELETE") {
+    handleDelete(request, response);
+  }
 })
+
+/*
+  Function for server-side calculated field
+  Sorts tasks in the server by their due date (newest first) 
+  then assigns them a priority based on their sorted index
+*/
+const assignItemPriorities = function() {
+    appdata.sort((a,b) => Date.parse(a.date) - Date.parse(b.date));
+
+    for(let i = 0; i < appdata.length; i++){
+      appdata[i].priority = i+1;
+    }
+}
 
 const handleGet = function( request, response ) {
   const filename = dir + request.url.slice( 1 ) 
 
   if( request.url === "/" ) {
     sendFile( response, "public/index.html" )
-  }else{
+  }
+  else if(request.url === "/appdata") {
+    sendAppData(response);
+  }
+  else{
     sendFile( response, filename )
   }
 }
@@ -40,15 +56,51 @@ const handlePost = function( request, response ) {
   })
 
   request.on( "end", function() {
-    console.log( JSON.parse( dataString ) )
+    // console.log( "received: ", JSON.parse( dataString ) )
+    let dataReceived = JSON.parse(dataString); // convert string received to JSON object
 
-    // ... do something with the data here!!!
+    let data = {
+      name: dataReceived.name,
+      date: dataReceived.date,
+      color: dataReceived.color
+    }
+
+    appdata.push(data); // store data received in the server
+
+    assignItemPriorities(); // server-side calculation performed here
+
+    // console.log("storage", appdata)
 
     response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
     response.end("test")
   })
 }
 
+const handleDelete = function( request, response ) {
+  let dataString = ""
+
+  request.on( "data", function( data ) {
+      dataString += data 
+  })
+
+  request.on( "end", function() {
+    // console.log( "received: ", dataString)
+    data = parseInt(dataString); // convert string received to JSON object
+    appdata.splice(data, 1); // remove 1 item at the index specified in the response body
+    assignItemPriorities();
+
+    response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
+    response.end("test")
+  })
+}
+
+// sends a json representation of the current server storage
+const sendAppData = function( response ) {
+  response.writeHeader( 200, { "Content-Type": "application/json" })
+  response.end( JSON.stringify(appdata) )
+}
+
+// sends specified file on the server, if found
 const sendFile = function( response, filename ) {
    const type = mime.getType( filename ) 
 
