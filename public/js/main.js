4389e7b245371475be70c548fd48d35dfb162df2
RyanNguyen321 <nguyenryan0903@gmail.com>
2024-03-24 23:33:41 -0400

    Finished Everything

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,144 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
+let selectedItem = 0;
+function screenToView() {
+    const makeWorkOutWindow = document.getElementById("make-workout-window");
+    const viewWorkoutWindow = document.getElementById("view-workout-window");
+    const editWorkoutWindow = document.getElementById("edit-workout-window");
+
+    if (editWorkoutWindow.style.display === "none" || makeWorkOutWindow.style.display === "none") {
+        makeWorkOutWindow.style.display = "none";
+        editWorkoutWindow.style.display = "none"
+        viewWorkoutWindow.style.display = "flex";
+    }
+}
+
+function screenToMake() {
+    const makeWorkOutWindow = document.getElementById("make-workout-window");
+    const viewWorkoutWindow = document.getElementById("view-workout-window");
+    const editWorkoutWindow = document.getElementById("edit-workout-window");
+
+    if (viewWorkoutWindow.style.display === "none" || editWorkoutWindow.style.display === "none") {
+        viewWorkoutWindow.style.display = "none";
+        editWorkoutWindow.style.display = "none"
+        makeWorkOutWindow.style.display = "flex";
+    }
+}
+
+function deleteWorkout() {
+    fetch("/" + selectedItem,
+        {
+            method: 'DELETE',
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    populateSidebar();
+    populateTable();
+    screenToMake();
+}
+
+function editWorkout() {
+    const title = document.getElementById("title-edit");
+    const desc = document.getElementById("description-edit");
+    fetch("/" + selectedItem,
+        {
+            method: 'POST',
+            body: JSON.stringify({
+                type: "edit",
+                title: title.value,
+                description: desc.value
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    populateSidebar();
+    populateTable();
+    screenToMake();
+}
+
+function screenToEdit(elementId) {
+    selectedItem = elementId;
+    const makeWorkOutWindow = document.getElementById("make-workout-window");
+    const viewWorkoutWindow = document.getElementById("view-workout-window");
+    const editWorkoutWindow = document.getElementById("edit-workout-window");
+
+    if (makeWorkOutWindow.style.display === "none" || viewWorkoutWindow.style.display === "none") {
+        makeWorkOutWindow.style.display = "none";
+        viewWorkoutWindow.style.display = "none";
+        editWorkoutWindow.style.display = "flex"
+    }
+
+    fetch("/get_workouts")
+        .then(res => res.json())
+        .then(data => {
+            const workout = data[elementId];
+            const titleEntry = document.getElementById("title-edit");
+            const descEntry = document.getElementById("description-edit");
+            titleEntry.value = workout.title;
+            descEntry.value = workout.description;
+        })
+        .catch(error => {
+            console.error(error);
+        })
+}
+
+function populateTable() {
+  fetch("/get_workouts")
+      .then(res => res.json())
+      .then(data => {
+        const tableBody = document.getElementById("table-body");
+        tableBody.innerHTML = ""
+        data.forEach(workout => {
+          const row = tableBody.insertRow();
+
+          const title = row.insertCell(0);
+          const desc = row.insertCell(1);
+          const time = row.insertCell(2);
+
+          title.textContent = workout.title;
+          desc.textContent = workout.description;
+          time.textContent = workout.time;
+        })
+
+      })
+      .catch(error => {
+        console.error("ERROR: " + error);
+      })
+}
+
+function populateSidebar() {
+  fetch("/get_workouts")
+      .then(res => res.json())
+      .then(data => {
+        const sidebar = document.getElementById("workout-list");
+        let id = 0;
+        sidebar.innerHTML = "";
+        data.forEach(workout => {
+          const sidebarElement = document.createElement('div');
+          sidebarElement.classList.add('sidebar-entry');
+          sidebarElement.innerText = workout.title;
+          sidebarElement.id = id;
+          sidebarElement.addEventListener("click", () => {
+              screenToEdit(sidebarElement.id);
+          })
+          id +=1;
+          sidebar.appendChild(sidebarElement);
+        })
+
+      })
+      .catch(error => {
+        console.error("ERROR: " + error);
+      })
+}
 
 const submit = async function( event ) {
   // stop form submission from trying to load
@@ -7,59 +147,49 @@ const submit = async function( event ) {
   // remains to this day
   event.preventDefault()
 
-  // id='yourname'
-  const titleInput = document.querySelector( "#title")
-  const descInput = document.querySelector( "#description")
-  const json = {
-          title: titleInput.value,
-          descInput: descInput.value
-      }
-  const body = JSON.stringify( json )
+  const titleInput = document.getElementById( "title" ),
+      descInput = document.getElementById("description")
+      json = {
+        title: titleInput.value,
+        description: descInput.value
+      },
+      body = JSON.stringify( json )
 
   const response = await fetch( "/submit", {
     method:"POST",
-    body 
+    body
   })
-      .then(response => response.json())
-      .then(json => console.log(json))
 
+    titleInput.value = ""
+    descInput.value = ""
   const text = await response.text()
-
-  console.log( "text:", text )
-}
-
-function screenToView() {
-  const makeWorkOutWindow = document.getElementById("make-workout-window");
-  const viewWorkoutWindow = document.getElementById("view-workout-window");
-
-  if (makeWorkOutWindow.style.display === "none") {
-    viewWorkoutWindow.style.display = "none";
-    makeWorkOutWindow.style.display = "flex";
-  }
-}
-
-function screenToMake() {
-  const makeWorkOutWindow = document.getElementById("make-workout-window");
-  const viewWorkoutWindow = document.getElementById("view-workout-window");
-
-  if (viewWorkoutWindow.style.display === "none") {
-    makeWorkOutWindow.style.display = "none";
-    viewWorkoutWindow.style.display = "flex";
-  }
+  populateTable();
+  populateSidebar();
 }
 
 window.onload = function() {
+  populateTable();
+  populateSidebar();
+  const button = document.getElementById("submit-button");
   const newListButton = document.getElementById("make-workout");
   const viewWorkoutButton = document.getElementById("view-workout");
-  const submitButton = document.getElementById("submit-button");
-  // submitButton.onclick = submit();
-  submitButton.addEventListener("click", () => {
-    submit();
-  })
+  const editButton = document.getElementById("edit-button");
+  const deleteButton = document.getElementById("delete-button");
   newListButton.addEventListener("click", () => {
-    screenToView()
-  })
-  viewWorkoutButton.addEventListener("click", () => {
     screenToMake()
-  })
+  });
+  viewWorkoutButton.addEventListener("click", () => {
+    screenToView()
+  });
+    editButton.addEventListener("click", () => {
+        editWorkout()
+    });
+    deleteButton.addEventListener("click", () => {
+        deleteWorkout()
+    });
+
+
+  button.onclick = submit;
 }
+
+
--- server.improved.js
+++ server.improved.js
@@ -1,24 +1,22 @@
 const http = require( "http" ),
-      fs   = require( "fs" ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library if you"re testing this on your local machine.
-      // However, Glitch will install it automatically by looking in your package.json
-      // file.
-      mime = require( "mime" ),
-      dir  = "public/",
-      port = 3000
+    fs   = require( "fs" ),
+    // IMPORTANT: you must run `npm install` in the directory for this assignment
+    // to install the mime library if you"re testing this on your local machine.
+    // However, Glitch will install it automatically by looking in your package.json
+    // file.
+    mime = require( "mime" ),
+    dir  = "public/",
+    port = 3000
 
-const appdata = [
-  { "model": "toyota", "year": 1999, "mpg": 23 },
-  { "model": "honda", "year": 2004, "mpg": 30 },
-  { "model": "ford", "year": 1987, "mpg": 14} 
-]
-// Create a server
+const appdata = []
+
 const server = http.createServer( function( request,response ) {
   if( request.method === "GET" ) {
-    handleGet( request, response )    
+    handleGet( request, response )
   }else if( request.method === "POST" ){
-    handlePost( request, response ) 
+    handlePost( request, response )
+  }else if (request.method === "DELETE") {
+    deletePost(request, response)
   }
 })
 
@@ -26,46 +24,68 @@ const handleGet = function( request, response ) {
   const filename = dir + request.url.slice( 1 )
   if( request.url === "/" ) {
     sendFile( response, "public/index.html" )
-  }else{
+  }
+  else if (request.url === "/get_workouts"){
+    response.end(JSON.stringify(appdata));
+  }
+  else{
     sendFile( response, filename )
   }
 }
 
 const handlePost = function( request, response ) {
   let dataString = ""
-  // every time you get a new packet of data, appened data
   request.on( "data", function( data ) {
-      dataString += data 
+    dataString += data
   })
-  // once you get an "end packet" or end of data stream, do something with it
+
   request.on( "end", function() {
-    console.log( JSON.parse( dataString ) )
-    // ... do something with the data here!!!
-    response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
-    response.end("test")
+    const jsonObj = (JSON.parse(dataString));
+
+    if (jsonObj.type === "edit") {
+      const id  = request.url.substring(1);
+      appdata[id].title = jsonObj.title;
+      appdata[id].description = jsonObj.description;
+    } else {
+      // ... do something with the data here!!!
+      // Create a Date/Time of when the thing is created
+      let time = new Date();
+      let timeString = String(time.getMonth() + 1) + "/" + String(time.getDate()) + " " + String(time.getHours()) + ":" + String(time.getMinutes());
+      jsonObj.time = timeString;
+      appdata.push(jsonObj);
+
+      response.writeHead( 200, "OK", {"Content-Type": "text/plain" });
+      response.end((JSON.stringify(jsonObj)));
+    }
   })
 }
 
 const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+  const type = mime.getType( filename )
 
-   fs.readFile( filename, function( err, content ) {
+  fs.readFile( filename, function( err, content ) {
 
-     // if the error = null, then we"ve loaded the file successfully
-     if( err === null ) {
+    // if the error = null, then we"ve loaded the file successfully
+    if( err === null ) {
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { "Content-Type": type })
-       response.end( content )
+      // status code: https://httpstatuses.com
+      response.writeHeader( 200, { "Content-Type": type })
+      response.end( content )
 
-     }else{
+    }else{
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( "404 Error: File Not Found" )
+      // file not found, error code 404
+      response.writeHeader( 404 )
+      response.end( "404 Error: File Not Found" )
 
-     }
-   })
+    }
+  })
 }
+
+const deletePost = function (response) {
+  const id  = response.url.substring(1);
+  appdata.splice(id,1);
+}
+
 
 server.listen( process.env.PORT || port )
